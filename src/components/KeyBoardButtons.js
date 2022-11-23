279b76e1ab34691a29854b082d34b06e14175fe7
Renan Larios <renancarmona94@live.com>
2022-11-23 10:19:38 -0300

    Testing keyboard press

--- src/components/KeyBoardButtons.js
+++ src/components/KeyBoardButtons.js
@@ -1,16 +1,24 @@
-import { useEffect } from "react"
+import { useEffect, useCallback } from "react"
+
+
+ //refactor this
+ const numbers = ["0","1","2","3","4","5","6","7","8","9"]
+ const operators = [".", "+", "-", "*", "/", "="]
+ let shortCutButtons = ["Enter", "Delete", "Backspace","R", "r"]
 
 export const KeyboardButtons = (props) =>{
 
-    //refactor this
-    const numbers = ["0","1","2","3","4","5","6","7","8","9"]
-    const operators = [".", "+", "-", "*", "/", "="]
-    let shortCutButtons = ["Enter", "Delete", "Backspace","R", "r"]
-    
-    
     const validKeys = (value) =>{
         return numbers.includes(value) || operators.includes(value) || shortCutButtons.includes(value) 
     }
+
+    const handleKeyPress = useCallback((event) => {
+        // Call updateCalc here
+        if(validKeys(event.key)) {
+            detectPressedKey(event.key)
+        }
+        console.log(`Key pressed: ${event.key}`);
+      }, []);
     
     const detectPressedKey = (e) =>{
         
@@ -25,31 +33,22 @@ export const KeyboardButtons = (props) =>{
         if(e.toUpperCase() === 'R'){
             e = 'AC'
         }
-        
-        console.log(e)
-
+    
         document.getElementById(e).click()
     } 
     
-        useEffect(() => {
-            return () => {
-                const handleEvent = event =>{
-                    const {key} = event
-                    if(validKeys(key)) {
-                        detectPressedKey(key)
-                    }
-                }
-        
-                document.addEventListener('keydown', handleEvent, true)
-                document.removeEventListener('keydown', handleEvent)
-            }
-        }, [])
-        
-        return (
-            <>
-             {props.children}
-            </>
-        )
-    }
+    useEffect(() => {
+        return () => {
+            document.addEventListener('keydown', handleKeyPress, true)
+            document.removeEventListener('keydown', handleKeyPress)
+        }
+    }, [handleKeyPress])
+    
+    return (
+        <>
+            {props.children}
+        </>
+    )
+}
 
 export default KeyboardButtons
